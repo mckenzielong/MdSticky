65e2047d102cb7c336c73b25bbbd63f87f18cc3c
McKenzie Long <mckenzie.long0@gmail.com>
2015-08-04 16:37:51 -0600

    :shipit: Replace console.log with $log, cleanup whitespace

--- mdSticky.js
+++ mdSticky.js
@@ -1,9 +1,9 @@
 (function () {
 	angular.module('mdSticky', ['ngMaterial'])
-	.directive('mdSticky', MdStickyDirective);
-	
-	MdStickyDirective.$inject = ['$mdSticky', '$compile'];
-	function MdStickyDirective($mdSticky, $compile) {
+		.directive('mdSticky', MdStickyDirective);
+
+	MdStickyDirective.$inject = ['$mdSticky', '$compile', '$log'];
+	function MdStickyDirective($mdSticky, $compile, $log) {
 		return {
 			restrict: 'A',
 			replace: true,
@@ -11,23 +11,22 @@
 			template: '<div class="md-sticky-content"></div>',
 			compile: MdStickyLink
 		};
-		
+
 		function MdStickyLink(element, attrs, transclude) {
 			return function postLink(scope, element, attr) {
 				var outerHTML = element[0].outerHTML;
-				console.log(outerHTML);
+				$log.debug(outerHTML);
 				transclude(scope, function (clone) {
-					console.log(clone);
+					$log.debug(clone);
 					element.append(clone);
 				});
-				
+
 				transclude(scope, function (clone) {
 					var stickyClone = $compile(angular.element(outerHTML).removeAttr("md-sticky"))(scope);
 					stickyClone.append(clone);
 					$mdSticky(scope, element, stickyClone);
 				});
 			};
-			
 		}
 	}
 })();
